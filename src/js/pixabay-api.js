adeb2d6e2ccc045fb6487c3c581117cf6036eba9
Oleksandr Shamoiev <wamoev@gmail.com>
2026-05-02 22:34:12 +0300

    Use async/await

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,19 +1,18 @@
 import axios from 'axios';
 
-export const getImagesByQuery = (query, page) => {
-  return axios
-    .get('https://pixabay.com/api/', {
-      timeout: 5000,
-      params: {
-        key: '55620787-7366754b0519359a7474d5ac8',
-        image_type: 'photo',
-        orientation: 'horizontal',
-        safesearch: true,
-        q: query,
-      },
-    })
-    .then(response => response.data)
-    .catch(() => {
-      throw new Error('Something went wrong while fetching images');
-    });
+export const  getImagesByQuery = async (query, page) => {
+  const requestParams = {
+    key: '55620787-7366754b0519359a7474d5ac8',
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    q: query,
+  };
+
+  const { data } = await axios.get('https://pixabay.com/api/', {
+    timeout: 5000,
+    params: requestParams,
+  });
+
+  return data;
 };
--- src/main.js
+++ src/main.js
@@ -22,36 +22,39 @@ const showErrorNotification = message => {
   });
 };
 
-const onSearchFormSubmit = () => event => {
-  event.preventDefault();
-  const { target: searchForm } = event;
-  const queryString = searchForm.elements['search-text'].value.trim();
+const onSearchFormSubmit = () => async event => {
+  try {
+    event.preventDefault();
+    const { target: searchForm } = event;
+    const queryString = searchForm.elements['search-text'].value.trim();
 
-  if (!queryString) {
-    searchForm.elements['search-text'].value = '';
-    showErrorNotification('Please enter a search query');
-    return;
+    if (!queryString) {
+      searchForm.elements['search-text'].value = '';
+      showErrorNotification('Please enter a search query');
+      return;
+    }
+
+    clearGallery();
+    showLoader();
+
+    const data = await getImagesByQuery(queryString);
+
+    console.log(data);
+
+    if (data.hits.length === 0) {
+      showErrorNotification(
+        'Sorry, there are no images matching your search query. Please try again!'
+      );
+      return;
+    }
+
+    createGallery(data.hits);
+  } catch (error) {
+    console.error(error);
+    showErrorNotification('Something went wrong. Please try again later!');
+  } finally {
+    hideLoader();
   }
-
-  clearGallery();
-  showLoader();
-
-  getImagesByQuery(queryString)
-    .then(data => {
-      if (data.hits.length === 0) {
-        showErrorNotification(
-          'Sorry, there are no images matching your search query. Please try again!'
-        );
-        return;
-      }
-
-      createGallery(data.hits);
-    })
-    .catch(error => {
-      console.error(error);
-      showErrorNotification('Something went wrong. Please try again later!');
-    })
-    .finally(() => hideLoader());
 };
 
 refs.form.addEventListener('submit', onSearchFormSubmit());
